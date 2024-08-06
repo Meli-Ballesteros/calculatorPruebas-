e838e9ac78b9e1f3b058fcfe3799560e9097f006
Melissa Ballesteros <1061684538@u.icesi.edu.co>
2024-08-06 17:51:22 -0500

    create method and menu whit calculator

---
+++ src/main/java/model/project.java
@@ -0,0 +1,12 @@
+package model;
+
+public class project {
+
+    public int addition(int a, int b){
+        return a+b;
+    }
+    public int subtraction(int a, int b){
+        return a-b;
+    }
+
+}
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,4 +1,75 @@
 package ui;
+import model.project;
+import java.util.Scanner;
 
 public class Main {
+    Scanner sc = new Scanner(System.in);
+    project calculator;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.menu();
+    }
+    public Main() {
+        calculator = new project();
+    }
+
+    public void menu() {
+        int option = 0;
+        do{
+            System.out.println("\nWelcome to calculator");
+            System.out.println("-----------------------");
+            System.out.println("1. addition");
+            System.out.println("2. subtraction");
+            System.out.println("3. multiplication");
+            System.out.println("4. division");
+            System.out.println("**********");
+            System.out.println("5. Exit");
+            option = sc.nextInt();
+
+            switch(option){
+                case 1:
+                    addition();
+                    break;
+                case 2:
+                    subtraction();
+                    break;
+                case 3:
+                    // multiplication();
+                    break;
+                case 4:
+                    //division();
+                    break;
+                case 5:
+                    System.out.println("See u later");
+                    break;
+            }
+
+
+        }while (option != 5);
+    }
+
+    public void addition(){
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+        sc.nextLine();
+
+        int result = calculator.addition(a,b);
+        System.out.println("the result is: "+ result);
+    }
+
+    public void subtraction(){
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter second number");
+        int b =sc.nextInt();
+        sc.nextLine();
+
+        int result = calculator.subtraction(a,b);
+        System.out.println("the result is:"+ result);
+    }
 }
